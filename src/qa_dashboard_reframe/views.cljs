d742eef1ccf2077e00bbf854726bbbeff759b6b9
leo crapart <leocrapart@yahoo.fr>
2022-06-21 12:06:43 +0200

    get tests run log

--- src/qa_dashboard_reframe/views.cljs
+++ src/qa_dashboard_reframe/views.cljs
@@ -14,6 +14,42 @@
 (defn closed-pbis [])
 
 
+(defn last-build-id []
+	39655)
+
+
+
+(defn timeline [build-id]
+	(let [url (str "https://dev.azure.com/hagerdevops-prod/Platform/_apis/build/builds/" build-id "/Timeline")]))
+
+(defn is-run-integration-test-job [record]
+	(let [type (record :type)
+				name (record :name)]
+		(if (and (= (type "Job"))
+				   	 (= (name "Run integration tests on DEV")))
+			true
+			false)))
+
+
+(defn run-log-url [timeline]
+	(let [records (timeline :records)
+				job (filter is-run-integration-test-job records)]
+		(get-in job [:log :url])))
+
+(defn raw-run-log [run-log-url])
+
+(defn parsed-run-log [raw-run-log]
+	{:passed 19
+	 :failed 0
+	 :test-results-url ""})
+
+(defn test-results-url [parsed-run-log])
+(defn )
+
+(defn get-integration-tests-log []
+	(let [url "https://dev.azure.com/hagerdevops-prod/ae626ca0-d75b-47c4-bf08-faa63374577c/_apis/build/builds/39655/logs/29"]
+		"lalala"))
+
 (defn fetch-button []
 	[:button.bg-blue-300.px-4.py-2.rounded
 		{:on-click (fn [e]
